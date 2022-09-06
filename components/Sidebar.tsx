883570d9b5146dec2bf517c34739dc01de68784d
saeed-khazaei <saeedkhazaei94@gmail.com>
2022-09-06 12:15:35 +0430

    fix drawer position

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,16 +4,20 @@ import IconButton from '@mui/material/IconButton';
 import Drawer from '@mui/material/Drawer';
 
 import Menus from './Menus';
+import { useTheme } from '@mui/material/styles';
 
 const Sidebar = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
   const toggleDrawer = () => {
     setOpenSidebar(!openSidebar);
   };
+
+  const theme = useTheme();
+
   return (
     <>
       <Drawer
-        anchor="left"
+        anchor={theme.direction == 'ltr' ? 'right' : 'left'}
         open={openSidebar}
         onClose={(_) => toggleDrawer()}
         sx={{ width: '240px' }}
